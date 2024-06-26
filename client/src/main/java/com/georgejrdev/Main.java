9ab430a1d69cedb3917aa8f4b52a5e65e571985c
NoxusJr <ctt.georgejr@gmail.com>
2024-06-26 02:36:54 -0300

    Call Screen in Main

---
+++ client/src/main/java/com/georgejrdev/Main.java
@@ -0,0 +1,20 @@
+package com.georgejrdev;
+
+import com.georgejrdev.screen.Frame;
+import com.georgejrdev.utils.LocalizeFlags;
+import com.georgejrdev.utils.support.Analyzer;
+import com.georgejrdev.utils.support.File;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Analyzer analyzer = new Analyzer();
+
+        File file = new File(analyzer);
+        
+        LocalizeFlags localizeFlags = new LocalizeFlags(file);
+
+        new Frame(localizeFlags);
+    }
+}
